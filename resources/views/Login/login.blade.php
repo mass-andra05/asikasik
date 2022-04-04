4dc4685398ef9c3ac97c71bc23f202ca6c761fb5
mass-andra05 <deandratisna@gmail.com>
2022-04-04 13:38:13 +0700

    update

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -20,7 +20,7 @@
   <!-- /.login-logo -->
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+      <a href="../../index2.html" class="h1"><b>Smart</b>Attadence</a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
