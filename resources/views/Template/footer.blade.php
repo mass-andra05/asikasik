588f8529e6d606bfba2f13d46ba1c525cd3b6fb3
mass-andra05 <deandratisna@gmail.com>
2022-04-04 09:20:14 +0700

    membahkan template adminlte dan memodifnya

---
+++ resources/views/Template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">SMART ATTANDENCE</a>.</strong> All rights reserved.
+  </footer>
